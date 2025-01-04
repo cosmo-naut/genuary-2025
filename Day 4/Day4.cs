fd39c9f1d32390d47a558a5d3d6f9b96caff11d9
Aidan Tesselaar <aidan.tesla@live.com>
2025-01-04 13:43:52 +1100

    day 4

---
+++ Day 4/Day4.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class Day4 : Control
+{
+    private Color Dark = new Color("202020");
+    private Color Light = new Color("333533");
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+}
